2ae4ba8fefc3d440c7cf2c97b9be2bc0ba450ff2
Munawer <117267127+Munawer2022@users.noreply.github.com>
2023-07-06 17:39:39 +0500

    commit

--- lib/core/utils/constants/images.dart
+++ lib/core/utils/constants/images.dart
@@ -1,7 +1,7 @@
 String department_doctors_profiles =
     'https://saharadigitalhealth.in/sahara_digital_health/public/';
-String category = 'assets/images/category.png';
-String doctor = 'assets/images/doctor.png';
+// String category = 'assets/images/category.png';
+// String doctor = 'assets/images/doctor.png';
 String no_data = 'assets/images/No_data.png';
 String search = 'assets/images/Search.png';
 String appointment = 'assets/images/appointment.png';
